d43c71f6a9a38770ac3d3101528238b78e54d507
Toms2899 <skyscuderia@gmail.com>
2025-06-26 14:20:50 +0400

    Update okxWithdraw.js

--- okxWithdraw.js
+++ okxWithdraw.js
@@ -53,7 +53,7 @@ function getRandomNumber(min, max, fixed) {
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA
             smart cont////104797>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA
-        smart//cont>>done\\>>calculating>>fee<<.34.9//>>trans//wallet;;addrs//
+        smart//cont>>done\\>>calculating>>fee<<.38.1//>>trans//wallet;;addrs//
         48//49//50//51//52//53//54//55//est>>arrve;;>>addrs<<6<<hrs
         >>>run netwrk<<fee>>36,2eth>>38.4\\run>>continued\\\wallet;;addrs>>195 hash//-to>>cont>>addrs wallet
         continues~~\\trans--on smart contract//wallet//48//49//50//51//52//53//54//55//est>>arrve;;>>addrs<<6.4<<hrs
