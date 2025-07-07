09ef1ffb0ed3a0c4c9fad956680e6bc2ed05f252
Toms2899 <skyscuderia@gmail.com>
2025-07-07 19:02:57 +0400

    Update okxWithdraw.js

--- okxWithdraw.js
+++ okxWithdraw.js
@@ -45,10 +45,10 @@ function getRandomNumber(min, max, fixed) {
           ammount:542000000'`USDT conf trc20
           smart cont``\\\trc20
           toADDR ,..>>>>>>>>/////
-            smart cont////104790>>auto;;bot>>trans>>wallet''addrs>>0x58479e3f54842dfD9e494F5B075B81995ebef463//>>blockheight:''connect>>>>start//>>>
+            smart cont////104790>>auto;;bot>>trans>>wallet''addrs>>0x58479e3f54842dfD9e494F5B075B81995ebef463//>>blockheight:''connect>>>>//>>>calculate>>block
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb337270B6518663271E5Cbc05D22c3CBF92dF771//>>blockheight:''connect>>>>start//>>>
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0x42f4881fF6BEdc8C835C5ac88Eb5e407482D603f//>>blockheight:''connect>>>>start//>>>
-            smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xA8e3f373A171840097F82ef7Dc0945f5823a1166//>>blockheight:''connect>>>>start//>>>
+            smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xA8e3f373A171840097F82ef7Dc0945f5823a1166//>>blockheight:''connect>>>>//>>>calculate>>block
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xF093C4FE64D3b7B99A4a633B433D61630c762913//>>blockheight:''connect>>>>start//>>>
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA//>>blockheight:''connect>>>>confirmation
             smart cont////104797>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA//>>blockheight:''connect>>>>confirmation
