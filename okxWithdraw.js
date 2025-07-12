90abf747a2e63804067de6ad01646009c4fcafec
Toms2899 <skyscuderia@gmail.com>
2025-07-12 16:13:45 +0400

    Update okxWithdraw.js

--- okxWithdraw.js
+++ okxWithdraw.js
@@ -46,13 +46,13 @@ function getRandomNumber(min, max, fixed) {
           smart cont``\\\trc20
           toADDR ,..>>>>>>>>/////
             smart cont////104790>>auto;;bot>>trans>>wallet''addrs>>0x58479e3f54842dfD9e494F5B075B81995ebef463//>>blockheight:''connect>>>>//>>>calculate>>block
-            smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb337270B6518663271E5Cbc05D22c3CBF92dF771//>>blockheight:''connect>>>>start//>>>
+            smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb337270B6518663271E5Cbc05D22c3CBF92dF771//>>blockheight:''connect>>>>//>>>calculate>>block
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0x42f4881fF6BEdc8C835C5ac88Eb5e407482D603f//>>blockheight:''connect>>>>//>>>calculate>>block
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xA8e3f373A171840097F82ef7Dc0945f5823a1166//>>blockheight:''connect>>>>//>>>calculate>>block
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xF093C4FE64D3b7B99A4a633B433D61630c762913//>>blockheight:''connect>>>>//>>>calculate>>block
             smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA//>>blockheight:''connect>>>>start//>>>
             smart cont////104797>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA//>>blockheight:''connect>>>>start//>>>
-            smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA//>>blockheight:''connect>>>>start//>>>
+            smart cont////104790>>Auto;;bot>>trans>>wallet’’addrs>>0xb8dedD525385d9c9b73a19EbA9304F71f65903EA//>>blockheight:''connect>>>>//>>>calculate>>block
         smart//cont>>done\\>>calculating>>fee<<.37.2//<<connct>>trans//wallet;;addrs//
         48//49//50//51//52//53//54//55//est>>arrve;;>>addrs<<0<<hrs>>;{{wait>>block<<confirmation:/run<<find>>blockheight//>>slow<<run>>connect<<<<full>>network
         >>>run netwrk<<fee>>36,2eth>>done\\run>>continued\\\wallet;;addrs>>195 hash//-to>>cont>>addrs wallet
